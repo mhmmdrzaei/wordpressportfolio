1dd244cacf09edd381abdb1796d8f6040601b700
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2016-03-09 09:51:46 -0500

    initial commit

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php get_header();  ?>
+
+<div class="main">
+  <div class="container">
+
+    <div class="content">
+      <?php // Start the loop ?>
+      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+        <?php the_content(); ?>
+
+      <?php endwhile; // end the loop?>
+    </div> <!-- /,content -->
+
+  </div> <!-- /.container -->
+</div> <!-- /.main -->
+
+<?php get_footer(); ?>
